8ffbff21386f05650f5f7ef1bb7a0a01a1158cc3
Edu Olivares <116183221+titodelas@users.noreply.github.com>
2023-02-27 14:53:08 +0100

    Find .jpg File

--- Shegram.py
+++ Shegram.py
@@ -1,4 +1,15 @@
 import instaloader
+import time, os
+
 bot = instaloader.Instaloader()
 username = input("Instert Instagram account: @")
 print(bot.download_profile(username,profile_pic_only=True))
+
+# Trying to detect the .jpg file lmao
+"""
+time.sleep(5)
+
+for file in os.listdir(f"/{username}"):
+    if file.endswith(".jpg"): # Instagram pfp save as .jpg
+        print(os.path.join(f"/{username}", file))
+"""
