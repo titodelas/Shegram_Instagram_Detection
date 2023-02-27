40ac8325b035b752ae404d83fa5e58190d0d3b4b
Edu Olivares <116183221+titodelas@users.noreply.github.com>
2023-02-27 18:20:59 +0100

    Trying to build the reverse lookup

--- Shegram.py
+++ Shegram.py
@@ -1,15 +1,18 @@
 import instaloader
 import time, os
+from GoogleSearch import Search
 
 bot = instaloader.Instaloader()
+path = os.getcwd() # Obtain the actual directory path
 username = input("Instert Instagram account: @")
 print(bot.download_profile(username,profile_pic_only=True))
 
-# Trying to detect the .jpg file lmao
-"""
-time.sleep(5)
+# Trying to detect the .jpg file
+time.sleep(1) # Whait time to create the folder with the pfp
 
-for file in os.listdir(f"/{username}"):
+for file in os.listdir(f"{path}/{username}"):
     if file.endswith(".jpg"): # Instagram pfp save as .jpg
-        print(os.path.join(f"/{username}", file))
-"""
+        print(os.path.join(f"{path}/{username}", file))
+
+output = Search(file_path=os.path.join(f"{path}/{username}", file))
+print(output)
