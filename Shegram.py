49962d4ef30ac0880a64a0723d8467a3fb540615
Edu Olivares <116183221+titodelas@users.noreply.github.com>
2023-02-28 22:03:09 +0100

    Little Detail

--- Shegram.py
+++ Shegram.py
@@ -19,8 +19,7 @@ Y88b.      888    888   Y88o88P   888    888 888    888      d88P888 88888b.d888
       "888 888    888     888     888    888 888 T88b     d88P   888 888  Y8P  888 
 Y88b  d88P 888    888     888     Y88b  d88P 888  T88b   d8888888888 888   "   888 
  "Y8888P"  888    888     888      "Y8888P88 888   T88b d88P     888 888       888 
-                                                                                   
-''')
+                                                                                   ''')
 
 def photo():
     global path
@@ -77,7 +76,7 @@ def username_finder():
 def menu():
     os.system('cls')
     print(banner)
-    print(f'''
+    print(f'''                       {Fore.YELLOW}She{Fore.RESET}rlock Insta{Fore.YELLOW}gram{Fore.RESET} Tool
     {Fore.RED}╔════════════════════════════╗╔═══════════════════════════════╗
     {Fore.RED}║{Fore.RESET} [{Fore.CYAN}1{Fore.RESET}] Fake Account Checker   {Fore.RED}║║{Fore.RESET} [{Fore.GREEN}</>{Fore.RESET}] Build By Edu Olivares   {Fore.RED}║
     {Fore.RED}║{Fore.RESET} [{Fore.CYAN}2{Fore.RESET}] Check SHYGRAM Database {Fore.RED}║║{Fore.RESET} [{Fore.BLUE}-?-{Fore.RESET}] This is the {v} version {Fore.RED}║
