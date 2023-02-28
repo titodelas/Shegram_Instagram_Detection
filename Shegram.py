5178445e92cecb52745d1c0177cfffe291644b56
Edu Olivares <116183221+titodelas@users.noreply.github.com>
2023-02-28 09:56:09 +0100

    Fix Code
    Now the code work

--- Shegram.py
+++ Shegram.py
@@ -12,26 +12,19 @@ def photo():
     username = input("Instert Instagram account: @")
     print(bot.download_profile(username,profile_pic_only=True))
 
-def read_id():
-    f = open(f"{path}/{username}/id", "r")
-    id = f.read()
-    os.remove(f'f"{path}/{username}/{username}_{id}.json.xz')
-    os.remove(f"{path}/{username}/id")
-
 def detect_file(): # Trying to detect the .jpg file
     global file
     time.sleep(1) # Whait time to create the folder with the pfp
 
     for file in os.listdir(f"{path}/{username}"):
         if file.endswith(".jpg"): # Instagram pfp save as .jpg
-            print(os.path.join(f"{path}/{username}", file))
+            # Rename the file to make easy the detection
+            os.rename(f'{path}/{username}/{file}', f'{path}/{username}/{username}.jpg')
         else:
-            print('File Not Found')
-
-
+            pass
 
 def reverse_lookup(): # Upload photo to Google Image Search
-    filePath = f'{path}/{username}/{file}'
+    filePath = f'{path}/{username}/{username}.jpg'
     searchUrl = 'http://www.google.hr/searchbyimage/upload'
     multipart = {'encoded_image': (filePath, open(filePath, 'rb')), 'image_content': ''}
     response = requests.post(searchUrl, files=multipart, allow_redirects=False)
@@ -39,4 +32,5 @@ def reverse_lookup(): # Upload photo to Google Image Search
     webbrowser.open(fetchUrl)
 
 photo()
-read_id()
+detect_file()
+reverse_lookup()
